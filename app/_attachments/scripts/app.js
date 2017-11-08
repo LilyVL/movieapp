7187ec611f824d627f28d4c1dd4b318fb0b0ec5a
LilyVL <s098448@ap.be>
2017-11-08 17:54:32 +0100

    Can check DB and view done

--- app/_attachments/scripts/app.js
+++ app/_attachments/scripts/app.js
@@ -5,10 +5,44 @@ var app = angular.module('movieApp', ['ngRoute']);
 
 app.controller('homeCtrl', function homeCtrl($scope) {
 	$("#searchButton").on('click', function(e) {
-		var nameValue = document.getElementById("actorentry").value.toLowerCase().replace(" ", "+");
 
-		$.getJSON("http://theimdbapi.org/api/find/person?name=" + nameValue, function(data, status){
-			document.write(JSON.stringify(data[0].filmography.actor));
-		});
-	})
+		var nameWithSpace = document.getElementById("actorentry").value.toLowerCase()
+		var nameValue = nameWithSpace.replace(" ", "+");
+
+
+
+		$.getJSON("http://localhost:5984/movies/_design/app/_view/byName?key=\"" + nameWithSpace + "\"", function(data, status){
+			if(parseInt(data.rows.length) >=1) {
+				document.write(JSON.stringify(data.rows[0].value));
+			}
+
+			else {
+				$.getJSON("http://theimdbapi.org/api/find/person?name=" + nameValue, function(data, status){
+
+					var newData = {
+						key : nameWithSpace,
+						movies : []
+					}
+
+					for(var i=0; i < data[0].filmography.actor.length; i++) {
+						newData.movies.push(data[0].filmography.actor[i].title);
+					}
+
+					var key = newData.key;
+					var movies = newData.movies;
+
+			        $.ajax({
+			           type: "POST",
+			           url: "../../",
+			           contentType: "application/json",
+			           data: JSON.stringify({key:key, movies:movies}),
+			            error: function(xhr, ajaxOptions, thrownError) {
+			               alert("Failed!");
+			           }
+
+			        });
+				});				
+			}
+		});	
+	});
 });
